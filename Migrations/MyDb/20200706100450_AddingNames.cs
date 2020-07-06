81f1906157db326ff3c3d06747fdb72677a50d7e
Amina Ait <45981655+aminait@users.noreply.github.com>
2020-07-06 16:47:38 +0300

    Add files via upload

---
+++ Migrations/MyDb/20200706100450_AddingNames.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Phonebook.Migrations.MyDb
+{
+    public partial class AddingNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
